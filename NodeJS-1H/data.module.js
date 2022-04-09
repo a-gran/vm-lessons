3eb21dd124ca55563436c61a831221c3e9dd6b8e
Aristarh Gran <aristarhgran@gmail.com>
2022-04-09 17:11:03 +0300

    modified

---
+++ NodeJS-1H/data.module.js
@@ -0,0 +1,3 @@
+const text = 'Hello NodeJS from data.js'
+
+export default text
--- NodeJS-1H/index.js
+++ NodeJS-1H/index.js
@@ -1,2 +1,17 @@
-console.log('-u')
-console.log('-u')
+import chalk from 'chalk'
+import text from './data.module.js'
+import __dirname from './data.module.js'
+import __filename from './data.module.js'
+import path from 'path'
+
+// console.log(chalk.blue(text))
+
+// console.log(__dirname)
+
+console.log('Название файла: ', path.basename(__filename))
+
+console.log('Имя директории: ', path.dirname(__filename))
+
+console.log('Расширение файла: ', path.extname(__filename))
+
+console.log('Расширение файла: ', path.parse(__filename))
